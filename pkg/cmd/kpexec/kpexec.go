fe37691aefbce066df23c43b8697af39d3b2ef39
ssup2 <supsup5642@gmail.com>
2021-03-24 17:36:08 +0000

    Apply client-go auth plugin

--- pkg/cmd/kpexec/kpexec.go
+++ pkg/cmd/kpexec/kpexec.go
@@ -20,6 +20,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
